334824a1304c3c20e93f23867fd728ebc158c645
amirreza13812003 <hosseiniseyyedamirreza2003@gmail.com>
2024-08-11 17:38:12 +0330

    add css and search functionality

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,8 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
+    item_name = request.GET.get('item_name')
+
+    if item_name != "" and item_name is not None:
+        products = products.filter(title__icontains=item_name) 
     return render(request, 'shop/index.html', {'products': products})
